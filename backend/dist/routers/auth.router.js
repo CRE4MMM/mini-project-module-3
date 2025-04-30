8014ee5b61ce8b439a8666340bf3159f2427880a
CRE4MMM <haidariqq90@gmail.com>
2025-04-30 11:49:31 +0700

    1.7.6 backend change

--- backend/dist/routers/auth.router.js
+++ backend/dist/routers/auth.router.js
@@ -5,9 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const auth_controller_1 = __importDefault(require("../controllers/auth.controller"));
-const rolecheck_1 = require("../middleware/rolecheck");
 const router = express_1.default.Router();
 router.post('/register', auth_controller_1.default.registCust);
-router.post('/register/org', (0, rolecheck_1.protectWithRole)(['ORGANIZER']), auth_controller_1.default.registOrg);
+router.post('/register/org', auth_controller_1.default.registOrg);
 router.post('/signin', auth_controller_1.default.signIn);
 exports.default = router;
--- backend/src/routers/auth.router.ts
+++ backend/src/routers/auth.router.ts
@@ -1,11 +1,10 @@
 import express from 'express'
 import authController from '../controllers/auth.controller'
-import { protectWithRole } from '../middleware/rolecheck'
 
 const router = express.Router()
 
 router.post('/register', authController.registCust)
-router.post('/register/org', protectWithRole(['ORGANIZER']), authController.registOrg)
+router.post('/register/org',authController.registOrg)
 router.post('/signin', authController.signIn)
 
 export default router
