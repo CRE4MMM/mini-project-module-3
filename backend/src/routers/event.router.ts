535af47a142d06ea3ffba9f8b7d238585c97c14d
CRE4MMM <haidariqq90@gmail.com>
2025-04-30 10:11:49 +0700

    1.7.1 tes

--- backend/src/routers/event.router.ts
+++ backend/src/routers/event.router.ts
@@ -1,9 +1,8 @@
 import express from 'express'
 import { createEvent } from '../controllers/event.controller'
-import { protectWithRole } from '../middleware/rolecheck'
 
 const router = express.Router()
 
-router.post('/create-event', protectWithRole(["ORGANIZER"]), createEvent)
+router.post('/create-event', createEvent)
 
 export default router
