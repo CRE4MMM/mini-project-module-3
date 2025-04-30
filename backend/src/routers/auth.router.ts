237f1a3f1aed97e9f67e96db0c1409dd707388da
CRE4MMM <haidariqq90@gmail.com>
2025-04-30 17:17:03 +0700

    1.8 verify account(typo mistake)

--- backend/src/routers/auth.router.ts
+++ backend/src/routers/auth.router.ts
@@ -6,6 +6,6 @@ const router = express.Router()
 router.post('/register', authController.registCust)
 router.post('/register/org',authController.registOrg)
 router.post('/signin', authController.signIn)
-router.get('verify', authController.verifyAccount)
+router.get('/verify', authController.verifyAccount)
 
 export default router
